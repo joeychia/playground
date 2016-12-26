6d39544e6ee3369aae6a6c3b613990f0202739fd
Joey Chia <Joey Chia>
2016-12-25 16:37:34 -0800

    461 accepted

---
+++ 461.HammingDistance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static int HammingDistance(int x, int y) {
+            int r = x ^ y;
+            int c = 0;
+            while (r > 0) {
+                if ((r & 1) == 1) {
+                    c++;
+                }
+                r >>= 1;
+            }
+            return c;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(HammingDistance(1, 4));
+        }
+    }
+}
