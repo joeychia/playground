3ff5cf6e3811006253f9c8a2d445fcc23cbbe50c
Joey Chia <Joey Chia>
2016-12-25 18:34:57 -0800

    'accpeted'

---
+++ 453.MinimumMovestoEqualArrayElements/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        // increase n - 1 by 1 equals decrease One element by 1
+        public static int MinMoves(int[] nums) {
+            var min = nums.Min();
+            return nums.Sum(n => n - min);
+
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(MinMoves(new int[]{1, 2, 3, 4}));
+        }
+    }
+}
