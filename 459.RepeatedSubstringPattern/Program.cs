2a653f9e1cbcfd48c2b2f09a74d53677118f392b
Joey Chia <Joey Chia>
2016-12-25 17:43:39 -0800

    most optimized

--- 459.RepeatedSubstringPattern/Program.cs
+++ 459.RepeatedSubstringPattern/Program.cs
@@ -5,30 +5,34 @@ namespace ConsoleApplication
     
     public class Program
     {
-        public static bool RepeatedSubstringPattern(string str) {
-            int[] prime = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
-            int length = str.Length;
-            for (int i = 0; i < prime.Length && prime[i] <= length; i++) {
-                if (length % prime[i] == 0) {
-                    string pattern = str.Substring(0, length / prime[i]);
-                    bool succ = true;
-                    for (int s = 1; s < prime[i] - 1; s++) {
-                        if (str.Substring(length / prime[i] * s, length / prime[i]) != pattern) {
-                            succ = false;
-                            break;
-                        }
-                    }
-                    if (succ) {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
+        // public static bool RepeatedSubstringPattern(string str) {
+        //     int[] prime = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+        //     int length = str.Length;
+        //     for (int i = 0; i < prime.Length && prime[i] <= length; i++) {
+        //         if (length % prime[i] == 0) {
+        //             string pattern = str.Substring(0, length / prime[i]);
+        //             bool succ = true;
+        //             for (int s = 1; s < prime[i]; s++) {
+        //                 if (str.Substring(length / prime[i] * s, length / prime[i]) != pattern) {
+        //                     succ = false;
+        //                     break;
+        //                 }
+        //             }
+        //             if (succ) {
+        //                 return true;
+        //             }
+        //         }
+        //     }
+        //     return false;
+        // }
 
+        // O(l)
+        public static bool RepeatedSubstringPattern(string str) {
+            return (str + str).Substring(1, str.Length * 2 - 2).Contains(str);
+        }
         public static void Main(string[] args)
         {
-            Console.WriteLine(RepeatedSubstringPattern("abac"));
+            Console.WriteLine(RepeatedSubstringPattern("aaaa"));
         }
     }
 }
